3822e86f5e5c254b57dd5969c666505303dabbf1
hsluoyz <hsluoyz@users.noreply.github.com>
2023-07-06 11:31:04 +0000

    deploy: 6690e97f097b706641908cd7ed2a51311c13fed4

--- casbin/prelude/sidebar-items.js
+++ casbin/prelude/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":[["Event",""],["EventData",""]],"struct":[["DefaultModel",""],["Enforcer","Enforcer is the main interface for authorization enforcement and policy management."],["FileAdapter",""],["Filter",""],["MemoryAdapter",""],["NullAdapter",""]],"trait":[["CoreApi",""],["EventEmitter",""],["IEnforcer",""],["InternalApi",""],["MgmtApi",""],["Model",""],["RbacApi",""],["TryIntoAdapter",""],["TryIntoModel",""]]};
+window.SIDEBAR_ITEMS = {};
